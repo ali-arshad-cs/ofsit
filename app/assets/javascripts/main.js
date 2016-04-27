3c6f54b5d347410be1b6bb62ace93884f32a8a63
Ali Arshad <ali@hqanalytics.net>
2016-04-28 02:19:03 +0500

    initial commit

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,30 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// the compiled file.
+//
+// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
+// GO AFTER THE REQUIRES BELOW.
+//
+//= require wp-includes/js/jquery/jqueryc1d8
+//= require jquery
+//= require wp-includes/js/jquery/jquery-migrate.min1576
+//= require wp-content/themes/healthflex/includes/core/assets/js/libs/modernizr/modernizr.custom.48287c1f9
+//= require wp-content/plugins/jetpack/modules/photon/photonb3d9
+//= require wp-content/plugins/contact-form-7/includes/js/jquery.form.mind03d
+//= require wp-content/plugins/contact-form-7/includes/js/scripts5b31
+//= require wp-content/themes/healthflex/includes/core/assets/js/libs/totop/jquery.ui.totopc1f9
+//= require wp-content/themes/healthflex/includes/core/assets/js/libs/owl.carousel.2.0.0-beta.2.4/owl.carousel.min7793
+//= require wp-content/themes/healthflex/assets/js/libs/bootstrap.minc1f9
+//= require wp-content/themes/healthflex/includes/core/assets/js/libs/easing/easing.minc1f9
+//= require wp-content/themes/healthflex/includes/core/assets/js/libs/wow/wow.minc1f9
+//= require wp-content/themes/healthflex/includes/core/assets/js/libs/conformity/dist/conformity.minc1f9
+//= require wp-content/themes/healthflex/assets/js/libs/particlesjs/particles.minc1f9
+//= require wp-content/themes/healthflex/includes/core/assets/js/libs/parallax/parallax.minc1f9
+//= require wp-content/themes/healthflex/assets/js/themec1f9
+//= require wp-includes/js/wp-embed.minc1f9
+//= require wp-content/plugins/js_composer/assets/js/dist/js_composer_front.min5859
