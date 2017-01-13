52ff58142b94917e6a0f419e861a10840d8d05da
Ali Arshad <aliarshad.cs@gmail.com>
2017-01-13 20:21:50 +0500

    added paperclip

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ActiveRecord::Base
+
+  has_attached_file :image, styles: { large: "900*500>", medium: "850x450" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
   validates :title, presence: true, length: { minimum: 5 }
   validates :body, presence: true
 end
